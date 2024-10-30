b41f583502784e7a408161b034f35bf2c84c3a2f
choco boi <aswii200@gmail.com>
2024-10-30 19:59:00 -0400

    enemy_changes

--- battle/enemy/enemy.gd
+++ battle/enemy/enemy.gd
@@ -2,13 +2,23 @@ extends CharacterBody3D
 
 class_name Enemy
 
-var JUMP_VELOCITY: float
-var health: float = 100
 var enemy_name: String = "Despacito"
+var health: float = 100
+var MAX_HEALTH: float = 100
+
 var is_banned = false
+
+enum PHASE {ATTACK , DEFEND, DED, MISC}
+
+var phase: PHASE = PHASE.DEFEND
+
+var anim_player: AnimationPlayer
+var animated: AnimatedSprite3D
 
 signal hurt_signal(amount:float)
 signal banned_signal
+signal animation_over_signal
+signal phase_switch_signal(phase:int)
 
 func _ready() -> void:
 	pass
@@ -34,3 +44,7 @@ func hurt(amount: float):
 func banned():
 	print(enemy_name+" ded")
 	is_banned = true
+
+func phase_switch(change: int):
+	phase = change
+	
--- battle/enemy/enemy_1.gd
+++ battle/enemy/enemy_1.gd
@@ -2,8 +2,8 @@ extends Enemy
 
 # Player 1 uses "jump_1" for jumping
 func _ready() -> void:
-	JUMP_VELOCITY = 4.5
 	health = 80
 	enemy_name = "witch"
-	connect("hurt", Callable(self, "hurt_me"))
-	connect("downed", Callable(self, "banned"))
+	
+	connect("hurt_signal", Callable(self, "hurt"))
+	connect("banned_signal", Callable(self, "banned"))
