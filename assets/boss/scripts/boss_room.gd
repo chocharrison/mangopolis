3375b7be0de79179edf4ffaf7d882b8d42f8b5d9
choco boi <aswii200@gmail.com>
2024-11-25 21:26:41 -0500

    boss modification

--- assets/boss/scripts/boss_room.gd
+++ assets/boss/scripts/boss_room.gd
@@ -13,6 +13,11 @@ func _process(delta: float) -> void:
 
 
 
-func _on_trigger_intro_area_entered(area: Area3D) -> void:
-	melonkitty.play_intro()
-	$trigger_intro.queue_free()
+func _on_trigger_intro_body_entered(body: Node3D) -> void:
+	if(body.name == "main_player"):
+		melonkitty.play_intro()
+		$trigger_intro.queue_free()
+
+
+func _on_button_pressed() -> void:
+	melonkitty.set_charge_start()
--- assets/boss/scripts/melonkitty.gd
+++ assets/boss/scripts/melonkitty.gd
@@ -1,17 +1,19 @@
 extends CharacterBody3D
 
 
-const SPEED = 1.0
+const SPEED = 60.0
 
-enum STATE {IDLE_WAIT,STAB,TIRED,CHARGE,IDLE,SMACKED,DISABLED,DEFAULT}
-@onready var state = STATE.DISABLED
+enum STATE {IDLE_WAIT,STAB,TIRED,CHARGE,IDLE,SMACKED,DISABLED,DEFAULT,CHARGE_START}
+@onready var state = STATE.IDLE
 
 var direction = Vector2(0,0) 
 var player_in_range = false
 var is_not_timer = true
+var locked_in = false
 
 @onready var anime: AnimationTree = get_node_or_null("AnimationTree")
 @onready var anime_player: AnimationPlayer = get_node_or_null("AnimationPlayer")
+@onready var line: Marker3D = get_node_or_null("line")
 
 @onready var stab: CollisionShape3D = $stab/stab
 @onready var cooldown: Timer = $Timer
@@ -33,17 +35,22 @@ func perform_state_actions(_delta):
 		STATE.IDLE_WAIT:
 			state_idle_wait()
 		STATE.CHARGE:
-			state_charge()
+			state_charge(_delta)
 		STATE.TIRED:
 			state_tired()
 		STATE.SMACKED:
 			state_smacked()
 		STATE.DISABLED:
 			state_disabled()
+		STATE.CHARGE_START:
+			state_charge_start()
 
 func handle_state_transitions(delta):
 	if state != STATE.STAB and state != STATE.CHARGE and state != STATE.TIRED:
 		stab.disabled = true
+	
+	if state != STATE.CHARGE:
+		velocity = Vector3(0,0,0)
 	
 	if player_in_range and is_not_timer:
 		SignalManager.hurt_signal.emit(10)
@@ -66,9 +73,16 @@ func state_stab(delta):
 	anime.get("parameters/playback").travel("stab")
 	global_position = global_position.lerp(player.global_position, 1 * delta)
 
-func state_charge():
-	pass
+func state_charge(delta):
+	anime.get("parameters/playback").travel("charge")
+	velocity.x = direction.x * SPEED
+	velocity.z = direction.z * SPEED
 	
+func state_charge_start():
+	if !locked_in:
+		set_sprite_direction(player.global_position)
+		line.look_at(player.global_position+direction, Vector3.UP)
+
 func state_tired():
 	set_sprite_direction_tired(player.global_position)
 	anime.get("parameters/playback").travel("tired")
@@ -78,6 +92,7 @@ func state_smacked():
 
 func set_sprite_direction(target_position: Vector3):
 	direction = (Vector3(target_position.x,0,target_position.z) - Vector3(global_position.x,0,global_position.z)).normalized()
+	print(direction)
 	anime.set("parameters/idle/BlendSpace2D/blend_position",Vector2(direction.x,-direction.z))
 	anime.set("parameters/stab/BlendSpace2D/blend_position",Vector2(direction.x,-direction.z))
 
@@ -98,6 +113,18 @@ func set_stab():
 	stab.disabled = false
 	state = STATE.STAB
 
+
+func set_charge():
+	stab.disabled = false
+	state = STATE.CHARGE
+
+func set_charge_start():
+	locked_in = false
+	anime.get("parameters/playback").travel("charge_start")
+	state = STATE.CHARGE_START
+
+func set_locked_in():
+	locked_in = true
 	
 func _on_stab_body_entered(body: Node3D) -> void:
 	if body.name == "main_player":
@@ -107,6 +134,9 @@ func _on_stab_body_entered(body: Node3D) -> void:
 			player_in_range = true
 			cooldown.wait_time = 0.5
 			cooldown.start()
+	if state == STATE.CHARGE:
+		pass
+		
 
 
 func _on_timer_timeout() -> void:
